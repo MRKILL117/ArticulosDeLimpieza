4676a5c6fb3aeb16042bdb8cf7c669d1c7a94f8c
Antonio Romano <aromanomacias@gmail.com>
2021-11-25 14:45:15 -0600

    changing packages names

--- src/modelo/User.java
+++ src/modelo/User.java
@@ -2,14 +2,13 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package back;
+package modelo;
 
 /**
  *
  * @author aroma
  */
-public class LendingList {
-    protected int userCode;
-    protected int articleCode;
-    protected String date;
+public class User {
+    protected int code;
+    protected String name;
 }
