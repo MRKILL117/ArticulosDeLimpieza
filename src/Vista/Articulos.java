43a32badda00a1bb4cba9446b4915ed866588989
LuisMa <mnuelgtz12@hotmail.com>
2021-11-25 18:14:21 -0600

    Revert "feli"
    This reverts commit 7cf19e0aa7589ead586ef3131f67c5b3ec0b0a2e.

--- src/Vista/Articulos.java
+++ src/Vista/Articulos.java
@@ -27,38 +27,17 @@ public class Articulos extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        lblFeli = new javax.swing.JLabel();
-        lblHappyFace = new javax.swing.JLabel();
-
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
-
-        lblFeli.setFont(new java.awt.Font("Tahoma", 0, 48)); // NOI18N
-        lblFeli.setText("Feli");
-
-        lblHappyFace.setFont(new java.awt.Font("Tahoma", 0, 36)); // NOI18N
-        lblHappyFace.setText(":D");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addGap(162, 162, 162)
-                .addComponent(lblFeli)
-                .addContainerGap(166, Short.MAX_VALUE))
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addComponent(lblHappyFace)
-                .addGap(180, 180, 180))
+            .addGap(0, 400, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addGap(69, 69, 69)
-                .addComponent(lblFeli)
-                .addGap(18, 18, 18)
-                .addComponent(lblHappyFace)
-                .addContainerGap(111, Short.MAX_VALUE))
+            .addGap(0, 300, Short.MAX_VALUE)
         );
 
         pack();
@@ -100,7 +79,5 @@ public class Articulos extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JLabel lblFeli;
-    private javax.swing.JLabel lblHappyFace;
     // End of variables declaration//GEN-END:variables
 }
